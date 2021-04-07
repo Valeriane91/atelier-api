41ac24e5a4900ff365843badbcf7144f3527cf6b
valeriane91 <jean.valerieanne@gmail.com>
2021-04-07 17:40:53 +0200

    axios

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,6 +6,7 @@ import SearchBar from 'src/components/SearchBar';
 import ReposResults from 'src/components/ReposResults';
 import Message from 'src/components/Message';
 
+import axios from 'axios';
 // import des données
 import reposData from 'src/data/repos';
 
@@ -13,11 +14,38 @@ import './styles.scss';
 
 // == Composant
 const App = () => {
+// chargement de la liste des repos git
+  const [repos, setRepos] = useState(reposData);
+
   const [inputReposLabel, setInputReposLabel] = useState([]);
 
   const addSearch = (newValue) => {
     setInputReposLabel(newValue);
   };
+
+  axios.get('https://api.github.com/search/repositories?q=REPOACHERCHER')
+    .then((response) => {
+      // exécuté quand la réponse arrive, si la réponse est un succès
+      console.log(response);
+
+      // on stocke dans le state (dans la case pour les articles) les articles récupérés
+      setRepos(response.data);
+    })
+    .catch((error) => {
+      // exécuté quand la réponse arrive, si la réponse est un échec
+      console.log(error);
+
+      // TODO il faudrait afficher l'information à l'utilisateur
+    })
+    // on peut utiliser finally ou un deuxième then
+    .finally(() => {
+      // exécuté après traitement de la réponse (après then ou catch)
+      console.log('finally');
+    });
+
+  console.log('On vient d\'envoyer la requête, on attend la réponse');
+  };
+
   return (
 
     <div className="app">
@@ -30,6 +58,7 @@ const App = () => {
       <Message />
     </div>
   );
+
 };
 
 // == Export
